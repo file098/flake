1d1747ef7254f6242ede9cd8ba8d8c061f991405
file098 <filippodigennaro98@outlook.it>
2022-09-17 19:19:47 +0200

    testing sway config

--- hosts/blade/default.nix
+++ hosts/blade/default.nix
@@ -8,6 +8,8 @@
     #Graphics
     "${self}/modules/desktop/gnome.nix"
     "${self}/modules/desktop/nvidia.nix"
+    "${self}/modules/desktop/sway.nix"
+
   ];
 
   boot = {
@@ -31,35 +33,6 @@
       };
     };
   };
-  
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.gdm.wayland = true;
-   programs.sway = {
-    enable = true;
-    wrapperFeatures.gtk = true; # so that gtk works properly
-    extraPackages = with pkgs; [
-      swaylock
-      swayidle
-      wl-clipboard
-      wf-recorder
-      mako # notification daemon
-      grim
-     #kanshi
-      slurp
-      alacritty # Alacritty is the default terminal in the config
-      dmenu # Dmenu is the default in the config but i recommend wofi since its wayland native
-    ];
-    extraSessionCommands = ''
-      export SDL_VIDEODRIVER=wayland
-      export QT_QPA_PLATFORM=wayland
-      export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
-      export _JAVA_AWT_WM_NONREPARENTING=1
-      export MOZ_ENABLE_WAYLAND=1
-    '';
-  };
-
-  programs.waybar.enable = true;
-
 
   networking = {
     hostName = "blade";
