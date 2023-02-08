0d205a5157ac6b621d1c1cf2d796c0a6be5c4275
file098 <filippodigennaro98@outlook.it>
2023-02-08 16:35:36 +0100

    removed customPkgs

--- flake.nix
+++ flake.nix
@@ -10,18 +10,18 @@
       inputs.nixpkgs.follows = "unstable";
     };
 
-    customPkgs = {
-      url = "path:./overlays/vim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # customPkgs = {
+    #   url = "path:./overlays/vim";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
   };
 
   # Function that tells my flake which to use and what do what to do with the dependencies.
-  outputs = inputs@{ self, nixpkgs, unstable, home-manager, customPkgs
-    , nixos-hardware, ... }:
+  outputs =
+    inputs@{ self, nixpkgs, unstable, home-manager, nixos-hardware, ... }:
 
     let
       user = "file0";
@@ -35,10 +35,10 @@
             ./nix.nix
             ./machines
             ./machines/blade
-            
-        nixos-hardware.nixosModules.common-pc-laptop-ssd
-        nixos-hardware.nixosModules.common-cpu-intel
-        nixos-hardware.nixosModules.common-gpu-intel
+
+            nixos-hardware.nixosModules.common-pc-laptop-ssd
+            nixos-hardware.nixosModules.common-cpu-intel
+            nixos-hardware.nixosModules.common-gpu-intel
             # this allows the use of unstable packages in the HM modules
             ({ ... }: {
               nixpkgs.overlays = [
